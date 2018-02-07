aaac4297c35b478734c39565eac062684dc63daa
meredithpyrich <meredithpyrich@gmail.com>
2018-02-06 21:35:33 -0500

    Add files via upload

--- scheduling.c
+++ scheduling.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-main()
+#define MAX_INPUT_SIZE 1000
+
+// argc - The number of arguments
+// argv - Array of the contents of each argument
+main(int argc, char ** argv)
 {
-
+	int i;
+	
+	// Check if an input file was given at the command line.
+	if (argc != 2)
+	{
+		printf("Invalid parameters.  The correct syntax is:\n\n");
+		printf("./scheduling processes.in\n\n");
+	}
+	
+	// Take the filename from the arguments and open the file
+	char * filename = argv[1];
+	FILE * ipf = NULL;
+	ipf = fopen(filename, "r");
+	
+	if (ipf != NULL)
+	{
+		// char holder - carries one line of the input file at a time.
+		char holder[MAX_INPUT_SIZE];
+		while(fgets(holder, MAX_INPUT_SIZE, ipf) != NULL)
+		{
+			// Print the input file (remove this later)
+			fprintf(stdout, "%s", holder);
+		} 
+		
+		fclose(ipf);
+	}
 }
 
 // First Come First Serve
