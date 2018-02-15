3283228565020ece532a5f63bbad17db527f6f29
meredithpyrich <meredithpyrich@gmail.com>
2018-02-15 17:05:06 -0500

    Add files via upload

--- scheduling.c
+++ scheduling.c
@@ -15,10 +15,16 @@ typedef struct process
 }process;
 
 void fcfs();
-void sjf();
 void rr(process * processes, int processCount, int runFor, int quantum);
 
-void bubbleSort(process * processes, int processCount);
+//Performs the scheduling function
+void sjf(process *queue, int * order, int processCount, int runFor);
+//Detects the next process
+int sjfSelect(process *queue, int size);
+//Updates the wait times after a given burst
+void updateWait(process *queue, int *wait, int burstSize, int currentProcess, int limit);
+
+void bubbleSort(process * processes, int * order, int processCount);
 
 // argc - The number of arguments
 // argv - Array of the contents of each argument
@@ -46,6 +52,7 @@ main(int argc, char ** argv)
 	int quantum = 0;
 
 	process * processes;
+	int * order;
 	
 	if (ipf != NULL)
 	{
@@ -79,6 +86,13 @@ main(int argc, char ** argv)
 					processCount = atoi(token);
 					
 					processes = malloc(sizeof(process) * processCount);
+					order = malloc(sizeof(int) * processCount);
+					
+					int count;
+					for (count = 0; count < processCount; count++)
+					{
+						order[count] = count;
+					}
 				}
 				else if (strcmp(token, "runfor") == 0)
 				{
@@ -89,9 +103,9 @@ main(int argc, char ** argv)
 				{
 					token = strtok(NULL, " ");
 
-					if (strcmp(token, "fcfs") == 0)
+					if (token[0] == 'f')
 						fcfsOn = 1;
-					else if (strcmp(token, "sjf") == 0)
+					else if (token[0] == 's')
 						sjfOn = 1;
 					else
 						rrOn = 1;
@@ -159,7 +173,8 @@ main(int argc, char ** argv)
 	}
 	else if (sjfOn)
 	{
-		sjf();
+		bubbleSort(processes, order, processCount);
+		sjf(processes, order, processCount, runFor);
 	}
 	else if (rrOn)
 	{
@@ -167,6 +182,7 @@ main(int argc, char ** argv)
 	}
 
 	free(processes);
+	free(order);
 }
 
 // First Come First Serve
@@ -176,19 +192,124 @@ void fcfs()
 }
 
 // Shortest Job First
-void sjf()
+void sjf(process *queue, int * order, int processCount, int runFor)
 {
+	//I believe that our output is a file. He says the name is supposed to be constant. I'm not sure
+	FILE *f = fopen("Processes.out", "w");
+	fprintf(f, "%d processes \n", processCount);
+	fprintf(f, "Using Shortest Job First (Pre)\n\n");
+	
+	int time = 0;
+	
+	//I use -1 to indicate the idle state
+	int currentProcess = -1;
+	//This is the index of the next process to arrive in the queue
+	int nextArrival = 0;
+	//I keep track of the burst to update the wait array when there is a context switch.
+	//That way I do not have to go through all of the processes at every time unit.
+	int currentBurst = 0;
+	
+	int *wait;
+	int *turnaround;
+	
+	//I use calloc here since I want everything to be initialized to 0
+	wait = calloc(processCount, sizeof(int));
+	turnaround = calloc(processCount, sizeof(int));
+	
+	for(time = 0; time < runFor; time++)
+	{	
+		//I check for new processes in a loop just in case there are multiple processes with the same arrival time
+		while(nextArrival < processCount && queue[nextArrival].arrival == time)
+		{
+			int nextProcess;
+			fprintf(f, "Time %d: %s arrived\n", time, queue[nextArrival].name); 
+			nextProcess = sjfSelect(queue, nextArrival + 1);
+			if(nextProcess != currentProcess)
+			{
+				if(nextProcess != -1)
+				{
+					updateWait(queue, wait, currentBurst, currentProcess, nextArrival);
+					currentBurst = 0;
+					currentProcess = nextProcess;
+					
+					fprintf(f, "Time %d: %s selected (burst %d)\n", time, queue[currentProcess].name, queue[currentProcess].burst);
+					turnaround[currentProcess] -= time;	
+				}
+			}
+			nextArrival++;
+		}
+		//A process with a burst time of 0 is considered done
+		if (queue[currentProcess].burst == 0)
+		{
+			fprintf(f, "Time %d: %s finished\n", time, queue[currentProcess].name);
+			turnaround[currentProcess] += time;
+			updateWait(queue, wait, currentBurst, currentProcess, nextArrival);
+			currentBurst = 0;
+			currentProcess = sjfSelect(queue, nextArrival);
+			if(currentProcess != -1)
+			{
+				fprintf(f, "Time %d: %s selected (burst %d)\n", time, queue[currentProcess].name, queue[currentProcess].burst);
+			}
+		}
+		if(currentProcess != -1)
+		{
+		queue[currentProcess].burst--;
+		currentBurst++;
+		}
+		else
+		{
+			fprintf(f, "Time %d: IDLE\n", time);
+		}
+	}
+	
+	fprintf(f, "Finished at time %d\n\n", time);
+	
+	int x;
+	for(x = 0; x < processCount; x++)
+	{
+		fprintf(f, "%s wait %d turnaround %d\n", queue[order[x]].name, wait[order[x]], turnaround[order[x]]);
+	}
+	
+	fclose(f);
+	free(wait);
+	free(turnaround);
+}
 
+int sjfSelect(struct process *queue, int size)
+{
+	int minBurst = INT_MAX;
+	int select = -1;
+	int x;
+	
+	for(x = 0; x < size; x++)
+	{
+		if(queue[x].burst < minBurst && queue[x].burst > 0)
+		{
+			minBurst = queue[x].burst;
+			select = x;
+		}
+	}
+	return select;
+}
+
+void updateWait(process *queue, int *wait, int burstSize, int currentProcess, int limit)
+{
+	int x;
+		for(x = 0; x < limit; x++)
+		{
+			if(x != currentProcess && queue[x].burst != 0)
+				wait[x] += burstSize;
+		}
 }
 
 // Round Robin
 void rr(process * processes, int processCount, int runFor, int quantum)
 {
+	FILE * f = fopen("Processes.out", "w");
+	fprintf(f, "%d processes \n", processCount);
+	fprintf(f, "Using Round-Robin\n");
+	fprintf(f, "Quantum 2\n\n");
 
-	printf("%d %s\n", processCount, "processes");
-	printf("Using Round Robin\n");
-	printf("%s %d\n\n", "Quantum", quantum);
-	
 	int i, j;
 	int arrived[processCount];
 	int arrivedTime[processCount];
@@ -210,7 +331,7 @@ void rr(process * processes, int processCount, int runFor, int quantum)
 
 			if (processes[j].arrival == i)
 			{
-				printf("%s %d: %s %s\n", "Time", i, processes[j].name, "arrived");
+				fprintf(f, "%s %d: %s %s\n", "Time", i, processes[j].name, "arrived");
 				arrived[j] = 1;
 				arrivedTime[j] = i;
 				burstTime[j] = processes[j].burst;
@@ -235,18 +356,18 @@ void rr(process * processes, int processCount, int runFor, int quantum)
 			
 			if (burstTime[cycle] == 0)
 			{
-				printf("%s %d: %s\n", "Time", i, "Idle");
+				fprintf(f, "%s %d: %s\n", "Time", i, "IDLE");
 			}
 			else
 			{
-				printf("%s %d: %s %s (%s %d)\n", "Time", i, processes[cycle].name, "selected", "burst", burstTime[cycle]);
+				fprintf(f, "%s %d: %s %s (%s %d)\n", "Time", i, processes[cycle].name, "selected", "burst", burstTime[cycle]);
 			}			
 
 			if (burstTime[cycle] < quantum && finished[cycle] != 1)	
 			{
 				remainingBurst = burstTime[cycle];
 				burstTime[cycle] = 0;
-				printf("%s %d: %s %s\n", "Time", i+remainingBurst, processes[cycle].name, "finished");
+				fprintf(f, "%s %d: %s %s\n", "Time", i+remainingBurst, processes[cycle].name, "finished");
 				finished[cycle] = 1;
 				finishedTime[cycle] = i;				
 			}		
@@ -268,18 +389,18 @@ void rr(process * processes, int processCount, int runFor, int quantum)
 		remainingBurst--;
 
 	}
-	printf("%s %d\n", "Finished at time", runFor);	
+	fprintf(f, "%s %d\n", "Finished at time", runFor);	
 
-	printf("\n");
+	fprintf(f, "\n");
 	for (i = 0; i < processCount; i++)
 	{
 		int turnaround = (finishedTime[i]-arrivedTime[i])+1;
 		
-		printf("%s %s %d %s %d\n", processes[i].name, "wait", (turnaround - processes[i].burst), "turnaround", turnaround);
+		fprintf(f, "%s %s %d %s %d\n", processes[i].name, "wait", (turnaround - processes[i].burst), "turnaround", turnaround);
 	}
 }
 
-void bubbleSort(process * processes, int processCount)
+void bubbleSort(process * processes, int * order, int processCount)
 {
     int curProcess1, curProcess2;
     for (curProcess1 = 0; curProcess1 < processCount; curProcess1++)
@@ -291,6 +412,10 @@ void bubbleSort(process * processes, int processCount)
 		process temp = processes[curProcess2];
 		processes[curProcess2] = processes[curProcess2+1];
 		processes[curProcess2+1] = temp;
+
+		int orderTemp = order[curProcess2];
+		order[curProcess2] = order[curProcess2+1];
+		order[curProcess2+1] = orderTemp;
 	    }
 	}
     }
