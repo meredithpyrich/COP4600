76b42c8cd44e4ada65cdd58be32d8e273e5f9c07
meredithpyrich <meredithpyrich@gmail.com>
2018-03-21 16:18:41 -0400

    Add files via upload

--- character.c
+++ character.c
@@ -5,18 +5,21 @@
 
 static int majorDeviceNumber;
 
-static int open();
-static int release();
-static ssize_t read();
-static ssize_t write();
+static int open(struct inode * iNode, struct file * File);
+static int release(struct inode * iNode, struct file * File);
+static ssize_t read(struct file * File, char __user * User, size_t Size, loff_t * Loff);
+static ssize_t write(struct file * File, const char __user * User, size_t Size, loff_t * Loff);
 
-static struct file_operations fileOps =
+//static struct class* Class = NULL;
+//static struct device* Device = NULL;
+
+/*static struct file_operations fileOps =
 {
-   .open = open,
-   .read = read,
-   .write = write,
-   .release = release,
-};
+	.open = open,
+	.release = release,
+	.read = read,
+	.write = write,
+};*/
 
 int init_module(void)
 {
@@ -29,24 +32,24 @@ void cleanup_module(void)
 	printk(KERN_INFO "Removing module\n");
 }
 
-static int open()
+static int open(struct inode * iNode, struct file * File)
 {
 	printk(KERN_INFO "Device opened\n");
 	return 0;
 }
 
-static int release()
+static int release(struct inode * iNode, struct file * File)
 {
 	printk(KERN_INFO "Device closed");
 	return 0;
 }
 
-static ssize_t read()
+static ssize_t read(struct file * File, char __user * User, size_t Size, loff_t * Loff)
 {
 
 }
 
-static ssize_t write()
+static ssize_t write(struct file * File, const char __user * User, size_t Size, loff_t * Loff)
 {
 
 }
